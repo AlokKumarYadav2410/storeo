e98be39258dfb025b0999f8f1481828dcb1cf07e
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-12 20:02:41 +0530

    Added animation using motion

---
+++ src/animations.js
@@ -0,0 +1,21 @@
+export const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+export const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      ease: 'easeOut',
+    },
+  },
+}
